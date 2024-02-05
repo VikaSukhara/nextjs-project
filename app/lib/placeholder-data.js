313f32cd390d82a599998dd4b7719c9a4b194459
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2024-02-05 15:17:53 +0200

    Add files via upload

---
+++ app/lib/placeholder-data.js
@@ -0,0 +1,17 @@
+// This file contains placeholder data that you'll be replacing with real data in the Data Fetching chapter:
+// https://nextjs.org/learn/dashboard-app/fetching-data
+const invoices = [
+  {
+    customer_id: customers[0].id,
+    amount: 15795,
+    status: 'pending',
+    date: '2022-12-06',
+  },
+  {
+    customer_id: customers[1].id,
+    amount: 20348,
+    status: 'pending',
+    date: '2022-11-14',
+  },
+  // ...
+];
